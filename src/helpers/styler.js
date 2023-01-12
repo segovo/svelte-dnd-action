46b4bcab64e5a727268062ed526ca4c115206166
James <mrsegovo@gmail.com>
2023-01-12 17:59:20 -0600

    4

--- src/helpers/styler.js
+++ src/helpers/styler.js
@@ -2,7 +2,7 @@ import {SHADOW_ELEMENT_ATTRIBUTE_NAME, DRAGGED_ELEMENT_ID} from "../constants";
 import {findCenter} from "./intersection";
 import {svelteNodeClone} from "./svelteNodeClone";
 
-const TRANSITION_DURATION_SECONDS = 0.1;
+const TRANSITION_DURATION_SECONDS = 0;
 
 /**
  * private helper function - creates a transition string for a property
@@ -42,7 +42,6 @@ export function createDraggedElementFrom(originalElement, positionCenterOnXY) {
     draggedEl.style.boxSizing = "border-box";
     draggedEl.style.height = `${rect.height}px`;
     draggedEl.style.width = `${rect.width}px`;
-    draggedEl.style.transition = `${trs("top")}, ${trs("left")}, ${trs("background-color")}, ${trs("opacity")}, ${trs("color")} `;
     // this is a workaround for a strange browser bug that causes the right border to disappear when all the transitions are added at the same time
     window.setTimeout(() => (draggedEl.style.transition += `, ${trs("width")}, ${trs("height")}`), 0);
     draggedEl.style.zIndex = "9999";
