59dbc4fc12d4af1978bc0e73f24f528ff4222e28
James <mrsegovo@gmail.com>
2023-02-10 17:14:01 -0600

    try 2

--- src/pointerAction.js
+++ src/pointerAction.js
@@ -36,7 +36,7 @@ import {areArraysShallowEqualSameOrder, areObjectsShallowEqual, toString} from "
 import {getBoundingRectNoTransforms} from "./helpers/intersection";
 
 const DEFAULT_DROP_ZONE_TYPE = "--any--";
-const MIN_OBSERVATION_INTERVAL_MS = 10;
+const MIN_OBSERVATION_INTERVAL_MS = 25;
 const MIN_MOVEMENT_BEFORE_DRAG_START_PX = 3;
 const DEFAULT_DROP_TARGET_STYLE = {
     outline: "rgba(255, 255, 102, 0.7) solid 2px"
@@ -96,10 +96,12 @@ function watchDraggedElement() {
     }
     window.addEventListener(DRAGGED_LEFT_DOCUMENT_EVENT_NAME, handleDrop);
     // it is important that we don't have an interval that is faster than the flip duration because it can cause elements to jump bach and forth
-    const observationIntervalMs = Math.max(
-        MIN_OBSERVATION_INTERVAL_MS,
-        ...Array.from(dropZones.keys()).map(dz => dzToConfig.get(dz).dropAnimationDurationMs)
-    );
+    // const observationIntervalMs = Math.max(
+    //     MIN_OBSERVATION_INTERVAL_MS,
+    //     ...Array.from(dropZones.keys()).map(dz => dzToConfig.get(dz).dropAnimationDurationMs)
+    // );
+    const observationIntervalMs = MIN_OBSERVATION_INTERVAL_MS; // is this true?
+
     observe(draggedEl, dropZones, observationIntervalMs * 1.07);
 }
 function unWatchDraggedElement() {
